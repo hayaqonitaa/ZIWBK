8f3af63ed72ccbfebd1e4d935ab387772725c25f
Geraldin Gysrawa <geraldingysrawa18@gmail.com>
2024-10-01 07:24:36 +0700

    screen hasil survey, navbar dan footer

--- resources/views/user_page/standar-pelayanan.blade.php
+++ resources/views/user_page/standar-pelayanan.blade.php
@@ -1,7 +1,3 @@
-@php
-$configData = Helper::appClasses();
-@endphp
-
 @extends('layouts/layoutMaster')
 
 @section('title', 'Landing - Front Pages')
@@ -44,40 +40,45 @@ $configData = Helper::appClasses();
       </div>
 
       <!-- Judul Standar Pelayanan -->
-      <div class="container text-center">
-        <h3 class="mb-4" style="border-bottom: 2px solid #000; display: inline-block;">Standar Pelayanan</h3>
+      <div class="container text-center" id="SPU">
+        <h3 class="mb-4">Standar Pelayanan</h3>
+        <hr> 
         <div class="row justify-content-center">
-          <div class="col-sm-6 col-lg-3 mb-4">
-            <div class="card border border-primary shadow-none h-100">
-              <div class="card-body text-center">
-                <img src="{{ asset('images/pendidikan.png') }}" alt="pendidikan" class="mb-2" style="width: 80px; height: auto;" />
-                <h5 class="h5">Pendidikan</h5>
-              </div>
+
+              <!-- Salinan Kepmen Nomor 433/M/2021 -->
+      <div class="col-sm-6 col-lg-3 mb-4" id="SalinanKepmenNo433tahun2021">
+      <a href="{{ asset('file/SalinanKepmenNo433tahun2021.pdf') }}" target="_blank">
+          <div class="card border border-primary shadow-none h-100">
+            <div class="card-body text-center">
+              <img src="{{ asset('images/pendidikan.png') }}" alt="pendidikan" class="mb-2" style="width: 80px; height: auto;" />
+              <h5 class="h5">Salinan Kepmen Nomor 433/M/2021</h5>
             </div>
           </div>
+        </a>
+      </div>
+
+          <!-- Standar Pelayanan Publik -->
           <div class="col-sm-6 col-lg-3 mb-4">
-            <div class="card border border-primary shadow-none h-100">
-              <div class="card-body text-center">
-                <img src="{{ asset('images/penelitian.png') }}" alt="penelitian" class="mb-2" style="width: 80px; height: auto;" />
-                <h5 class="h5">Penelitian</h5>
+          <a href="{{ asset('file/standarpelayananpublik.pdf') }}" target="_blank">
+              <div class="card border border-primary shadow-none h-100">
+                <div class="card-body text-center">
+                  <img src="{{ asset('images/pkm.png') }}" alt="pengabdian" class="mb-2" style="width: 80px; height: auto;" />
+                  <h5 class="h5">Standar Pelayanan Publik</h5>
+                </div>
               </div>
-            </div>
+            </a>
           </div>
+
+          <!-- SK Maklumat Pelayanan Publik -->
           <div class="col-sm-6 col-lg-3 mb-4">
-            <div class="card border border-primary shadow-none h-100">
-              <div class="card-body text-center">
-                <img src="{{ asset('images/pkm.png') }}" alt="pengabdian" class="mb-2" style="width: 80px; height: auto;" />
-                <h5 class="h5">Pengabdian Kepada Masyarakat</h5>
+            <a href="{{ asset('file/SKMaklumatPelayananPublik.pdf') }}" target="_blank">
+              <div class="card border border-primary shadow-none h-100">
+                <div class="card-body text-center">
+                  <img src="{{ asset('images/administrasi.png') }}" alt="administrasi" class="mb-2" style="width: 80px; height: auto;" />
+                  <h5 class="h5">SK Maklumat Pelayanan Publik</h5>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-lg-3 mb-4">
-            <div class="card border border-primary shadow-none h-100">
-              <div class="card-body text-center">
-                <img src="{{ asset('images/administrasi.png') }}" alt="administrasi" class="mb-2" style="width: 80px; height: auto;" />
-                <h5 class="h5">Administrasi</h5>
-              </div>
-            </div>
+            </a>
           </div>
         </div>
       </div>
