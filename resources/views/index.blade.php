79a7b7bcec937e56e1bab8b91cec06af8cd92f31
Geraldin Gysrawa <geraldingysrawa18@gmail.com>
2024-10-30 09:49:45 +0700

    Responsive UI footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
             <div class="mb-4">
                 <!-- Gambar Header dengan Penanda -->
                 <div class="header">
-                    <img src="{{ asset('images/Polban.png') }}" alt="headerpolban" class="img-fluid" />
+                    <img src="{{ asset('images/Polban.png') }}" alt="headerpolban" class="img-fluid w-100" />
 
                     <div class="text-box">
                         <!-- Kotak Kuning -->
@@ -49,55 +49,54 @@
     </div>
 </div>
 
-        <!-- Content Section -->
-        <section id="zonaIntegritas" class="section-py text-center">
-            <div class="container">
-                <h2>POLBAN Menuju Zona Integritas</h2>
-                <p>Transparansi. Akuntabilitas. Kualitas.<br><br><br></p>
+<!-- Content Section -->
+<section id="zonaIntegritas" class="section-py text-center">
+    <div class="container">
+        <h2 class="mb-4">POLBAN Menuju Zona Integritas</h2>
+        <p>Transparansi. Akuntabilitas. Kualitas.</p>
 
-                <!-- Three Columns Section -->
-                <div class="row mt-4">
-                    <div class="col-md-4">
-                        <h4>Pemerintahan Bersih</h4>
-                        <p>Mencegah korupsi <br>di setiap lini</p>
-                        <hr>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Pelayanan Prima</h4>
-                        <p>Cepat, tepat, dan <br>profesional</p>
-                        <hr>
-                    </div>
-                    <div class="col-md-4">
-                        <h4>Akuntabilitas</h4>
-                        <p>Hasil yang terukur dan <br>transparan</p>
-                        <hr>
-                    </div>
-                </div>
+        <!-- Three Columns Section -->
+        <div class="row mt-4">
+            <div class="col-12 col-md-4 mb-4 mb-md-0">
+                <h4>Pemerintahan Bersih</h4>
+                <p>Mencegah korupsi <br>di setiap lini</p>
+                <hr>
             </div>
-        </section>
-
-        <!-- Blue Box Section with bottom margin -->
-        <div class="container-fluid py-4" style="background-color: #E9EEF4; text-align: center; width: auto; margin-bottom: 20px;">
-            <p style="color: #003366; font-size: 1.2rem; line-height: 1.5;">
-                POLBAN berkomitmen untuk membangun lingkungan yang bersih dari<br>
-                korupsi dan memberikan layanan terbaik. Bersama, kita menciptakan sistem<br>
-                birokrasi yang efektif dan responsif untuk semua pemangku kepentingan.
-            </p>
-        </div>
-        
-        <!-- Full-Width Image and Text Section -->
-        <div class="container" style="margin-top: 100px;">
-            <div class="row align-items-center" id="piagamZiwbkWbbm">
-                <!-- Image Column -->
-                <div class="col-md-8">
-                    <img src="{{ asset('images/piagam.jpg') }}" alt="Piagam" class="img-fluid w-100" style="height: auto;" />
-                    <br> <br>
-                </div>
-                <!-- Text Column -->
-                <div class="col-md-4 text-md-right text-center mt-3 mt-md-0">
-                    <h4>Piagam ZIWBK/WBBM</h4>
-                </div>
+            <div class="col-12 col-md-4 mb-4 mb-md-0">
+                <h4>Pelayanan Prima</h4>
+                <p>Cepat, tepat, dan <br>profesional</p>
+                <hr>
+            </div>
+            <div class="col-12 col-md-4">
+                <h4>Akuntabilitas</h4>
+                <p>Hasil yang terukur dan <br>transparan</p>
+                <hr>
             </div>
         </div>
+    </div>
+</section>
+
+<!-- Blue Box Section with bottom margin -->
+<div class="container-fluid py-4" style="background-color: #E9EEF4; text-align: center; width: 100%; margin-bottom: 20px;">
+    <p class="mb-0" style="color: #003366; font-size: 1.2rem; line-height: 1.5;">
+        POLBAN berkomitmen untuk membangun lingkungan yang bersih dari<br>
+        korupsi dan memberikan layanan terbaik. Bersama, kita menciptakan sistem<br>
+        birokrasi yang efektif dan responsif untuk semua pemangku kepentingan.
+    </p>
+</div>
+
+<!-- Full-Width Image and Text Section -->
+<div class="container my-5">
+    <div class="row align-items-center" id="piagamZiwbkWbbm">
+        <!-- Text Column: Atur agar muncul di atas gambar pada layar kecil -->
+        <div class="col-12 col-md-4 text-center text-md-start order-1 order-md-2">
+            <h4>Piagam ZIWBK/WBBM</h4>
+        </div>
+
+        <!-- Image Column: Atur agar muncul di bawah teks pada layar kecil -->
+        <div class="col-12 col-md-8 order-2 order-md-1">
+            <img src="{{ asset('images/piagam.jpg') }}" alt="Piagam" class="img-fluid w-100 mb-3" />
+        </div>
+    </div>
+</div>
 @endsection
-
